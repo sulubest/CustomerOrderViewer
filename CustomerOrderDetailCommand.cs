0cc6e6168c770820c5c9b933358ea019b902cb46
Md Sultan Mahmud <88264767+sulubest@users.noreply.github.com>
2021-08-01 12:54:31 +0600

    Add files via upload

---
+++ CustomerOrderDetailCommand.cs
@@ -0,0 +1,65 @@
+﻿using CustomerOrderDetail.Models;
+using System;
+using System.Collections.Generic;
+using Oracle.ManagedDataAccess;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace CustomerOrderDetail.Repository
+{
+    class CustomerOrderDetailCommand
+    {
+        private string _connectionString;
+
+        //public OracleConnection _con =
+        //    new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString.ToString());
+
+        public CustomerOrderDetailCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IList<CustomerOrderDetailModel> GetList()
+        {
+            List<CustomerOrderDetailModel> customerOrderDetailModels = new List<CustomerOrderDetailModel>();
+
+            using (OracleConnection connection = new OracleConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (OracleCommand command = new OracleCommand("select t.customerorderid,t.customerid,t.itemid,t.firstname,t.lastname,t.description,t.price from  customerorderdetail t order by t.customerorderid", connection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                CustomerOrderDetailModel customerOrderDetailModel = new CustomerOrderDetailModel()
+                                {
+                                    CustomerOrderId = Convert.ToInt32(reader["customerorderid"]),
+                                    CustomerId = Convert.ToInt32(reader["customerid"]),
+                                    ItemId = Convert.ToInt32(reader["itemid"]),
+                                    FirstNmae = reader["firstname"].ToString(),
+                                    LastName=reader["lastname"].ToString(),
+                                    Description=reader["description"].ToString(),
+                                    Price=Convert.ToDecimal(reader["price"])
+                                };
+
+                                customerOrderDetailModels.Add(customerOrderDetailModel);
+
+                            //    string firstNmae = reader["firstname"].ToString();
+                            //    string firstNmae = reader["firstname"].ToString();
+                            //Console.WriteLine("first Name:",firstNmae.ToString());
+                            }
+                        }
+                }
+            }
+
+            return customerOrderDetailModels;
+        }
+    }
+}
---
+++ CustomerOrderDetailModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderDetail.Models
+{
+    class CustomerOrderDetailModel
+    {
+        public int CustomerOrderId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string FirstNmae { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
